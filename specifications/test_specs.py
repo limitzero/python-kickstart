f48bf42ae8c2e0e99ab803261c585ed766b292fa
Michael Calloway <Michael.Calloway@e-hps.com>
2015-12-17 10:54:27 -0600

    adding sample specifications file for testing

---
+++ specifications/test_specs.py
@@ -0,0 +1,10 @@
+"""
+module		: tests_specs.py
+description	: blank python test template for kickstarter
+"""
+class describe_tests:
+	def setup_class(self):
+		pass
+
+	def it_should_work(self):
+		assert True = True
